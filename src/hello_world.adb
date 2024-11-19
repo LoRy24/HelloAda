d1dcf849109c9184a6d401c317cf35cd2833a041
Lorenzo Rocca <51451299+LoRy24@users.noreply.github.com>
2024-11-19 21:20:25 +0100

    Added first IF

--- src/hello_world.adb
+++ src/hello_world.adb
@@ -1,11 +1,15 @@
 with Ada.Text_IO;
 
 procedure Hello_World is
-   Nome : String (1 .. 80);
-   FineNome : Natural;
+   Name : String (1 .. 80);
+   NameEnd : Natural;
 begin
    Ada.Text_IO.Put_Line ("What is your name? ");
    Ada.Text_IO.Put ("-> ");
-   Ada.Text_IO.Get_Line (Nome, FineNome);
-   Ada.Text_IO.Put_Line ("Hi " & Nome (1 .. FineNome) & "!");
+   Ada.Text_IO.Get_Line (Name, NameEnd);
+   Ada.Text_IO.Put_Line ("Hi " & Name (1 .. NameEnd) & "!");
+
+   if Name (1 .. NameEnd) = "Lorenzo" then
+      Ada.Text_IO.Put_Line ("Did you know that your name is the same" & " of my creator? Now you know it!");
+   end if;
 end Hello_World;
