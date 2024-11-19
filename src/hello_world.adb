9cf151461937e870df1b36809e4238e03964dc1c
Lorenzo Rocca <51451299+LoRy24@users.noreply.github.com>
2024-11-19 21:06:05 +0100

    Now the programs greets the user by its name

--- src/hello_world.adb
+++ src/hello_world.adb
@@ -1,6 +1,11 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;
 
 procedure Hello_World is
+   Nome : String (1 .. 80);
+   FineNome : Natural;
 begin
-   Put_Line("Hello, World!");
+   Ada.Text_IO.Put_Line ("What is your name? ");
+   Ada.Text_IO.Put ("-> ");
+   Ada.Text_IO.Get_Line (Nome, FineNome);
+   Ada.Text_IO.Put_Line ("Hi " & Nome (1 .. FineNome) & "!");
 end Hello_World;
